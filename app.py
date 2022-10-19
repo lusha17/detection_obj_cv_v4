785ab51396bb93fb014ec0959fa19680251f32cb
lusha17 <antonlushkin2307@yandex.ru>
2022-10-19 22:14:39 +0300

    add

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import streamlit as st
 import sys
 import gc
+sys.path.append('mediapipe')
 from detect_weapon import func_detect_weapon
+from streamlitMediapipe import mediapipe_f
+from detect_fire_smoke import func_detect_fire_smoke
 from streamlit.legacy_caching import clear_cache
 
 def main():
     gc.enable()
     st.header("Computer Vision Demo")
+
+    pages_1 = {
+        "Real time hand items detection": func_detect_weapon,
+        "Personality and action recognition": mediapipe_f,
+        "Real time fire and smoke detection": func_detect_fire_smoke
+    }
+    page_titles = pages_1.keys()
 
     st.sidebar.markdown("""<center data-parsed=""><img src="http://drive.google.com/uc?export=view&id=1Mad62XWdziqcx9wijUODpzGzqYEGhafC" align="center"></center>""",unsafe_allow_html=True,)
     st.sidebar.markdown(" ")
@@ -22,6 +32,13 @@ def main():
 
     if pages[1].button("Reload App"):
         reload()
-    func_detect_weapon()
+
+    page_title = st.sidebar.selectbox(
+        "Choose the app mode",
+        page_titles,
+    )
+    st.subheader(page_title)
+    page_func = pages_1[page_title]
+    page_func()
     
 main()
